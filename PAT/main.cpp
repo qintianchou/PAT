c4bdf6fdc494ad3860d582d44c888ecd83a5719d
Hao Zeng <zenghao1019@qq.com>
2018-04-27 21:31:47 +0800

    Initial Commit

---
+++ PAT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PAT
+//
+//  Created by Hao Zeng on 2018/4/27.
+//  Copyright © 2018年 Droidhen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
