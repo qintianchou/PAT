17797aa49baf082f31c859a630ecf3a5690b9233
Hao Zeng <zenghao1019@qq.com>
2018-05-02 09:05:27 +0800

    new target

---
+++ PAT/PAT1003.cpp
@@ -0,0 +1,19 @@
+//
+//  PAT1003.cpp
+//  PAT
+//
+//  Created by Hao Zeng on 2018/5/2.
+//  Copyright © 2018年 Droidhen. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+
+    cout << "test\n";
+    
+    return 0;
+}
---
+++ PAT1003/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PAT1003
+//
+//  Created by Hao Zeng on 2018/5/2.
+//  Copyright © 2018年 Droidhen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
