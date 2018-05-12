6e9337a8f69ea0e827af4a634b0211ab3e583233
Hao Zeng <zenghao1019@qq.com>
2018-05-12 11:51:58 +0800

    commit

--- SortTest/main.cpp
+++ SortTest/main.cpp
@@ -10,8 +10,32 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <cmath>
 
 using namespace std;
+
+class Point {
+public:
+    double x;
+    double y;
+    double r;
+    
+    Point(double x, double y, double r) {
+        this->x = x;
+        this->y = y;
+        this->r = r;
+    }
+    
+    static double dist(const Point& p1, const Point& p2) {
+        double dx = p1.x - p2.x;
+        double dy = p1.y - p2.y;
+        double d = sqrt(dx * dx + dy * dy);
+        d = d - p1.r - p2.r;
+        if (d < 0)
+            d = 0;
+        return d;
+    }
+};
 
 bool comp(const string& l, const string& r)
 {
@@ -19,6 +43,22 @@ bool comp(const string& l, const string& r)
 }
 
 int main(int argc, const char * argv[]) {
+    
+    freopen("in.txt", "r", stdin);
+    
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << a << b << c << d << endl;
+    
+    Point p1(0, 0, 0);
+    Point p2(1, 1, 1);
+    
+    double dist = Point::dist(p1, p2);
+    
+    cout << dist << endl;
+    
+    cout << -22/7 << endl;
+    cout << -22%7 << endl;
 
     vector<string> vec;
     vec.push_back("about about");
